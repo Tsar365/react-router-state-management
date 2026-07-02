530e2341de07cc7029c723dc680556d6218cee6a
Ehasanul Haque <ehasanmd365@gmail.com>
2026-07-02 22:22:03 +0600

    42-3 updated

--- react42/src/assets/components/navbar/NavBar.jsx
+++ react42/src/assets/components/navbar/NavBar.jsx
@@ -1,4 +1,7 @@
-import Link from "./Link";
+import Link from './Link';
+import { SquareMenu } from 'lucide-react';
+import { useState } from 'react';
+import { CircleX } from 'lucide-react';
 
 // import React from 'react';
 const navigationData = [
@@ -30,16 +33,36 @@ const navigationData = [
 ];
 
 const NavBar = () => {
-	return (
-		<nav>
-			<ul>
-				{navigationData.map((route) => (
-					<Link
-						key={route.id}
-						route={route}></Link>
-				))}
-			</ul>
+	const [open, setOpen] = useState(false);
 
+	const links = navigationData.map((route) => (
+		<Link
+			key={route.id}
+			route={route}></Link>
+	));
+
+	return (
+		<nav className="bg-green-800 p-4 text-white flex justify-between mx-10">
+			<span
+				className="flex items-center space-x-2"
+				onClick={() => setOpen(!open)}>
+				{open ? (
+					<CircleX className="md:hidden"></CircleX>
+				) : (
+					<SquareMenu className="md:hidden"></SquareMenu>
+				)}
+
+				<h3>My navbar</h3>
+			</span>
+			<ul>
+				{/* {navigationData.map((route) => (
+					 <Link
+						key={route.id}
+						 route={route}></Link>
+				 ))} */}
+
+				{links}
+			</ul>
 			<ul className="flex space-x-4 bg-red-800 p-4 text-white">
 				{navigationData.map((item) => (
 					<li key={item.id}>
@@ -47,7 +70,6 @@ const NavBar = () => {
 					</li>
 				))}
 			</ul>
-
 			<ul className="flex space-x-4 bg-gray-800 p-4 text-white">
 				<li className="mr-10">
 					<a href="#">Home</a>
@@ -59,6 +81,11 @@ const NavBar = () => {
 					<a href="/contact">Contact</a>
 				</li>
 			</ul>
+			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">click me</button>
+			
+      <ul classname='md:flex hidden'>
+        {links}
+      </ul>
 		</nav>
 	);
 };
